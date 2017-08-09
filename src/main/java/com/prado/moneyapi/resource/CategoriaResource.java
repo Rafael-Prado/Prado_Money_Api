3c1e79b04b03d6047444e31d554d6e8f855f1784
Rafae Prado <rafaeldacruzdoprado@hotmail.com>
2017-08-09 15:30:33 -0300

    configurando Basic Security para desenvolvimento

--- src/main/java/com/prado/moneyapi/resource/CategoriaResource.java
+++ src/main/java/com/prado/moneyapi/resource/CategoriaResource.java
@@ -30,7 +30,7 @@ public class CategoriaResource {
 	private CategoriaRepository categoriaRepository;	
 	
 	@GetMapping
-	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_CATEGORIA') and #oauth2.hasScope('read')")
+	//@PreAuthorize("hasAuthority('ROLE_PESQUISAR_CATEGORIA') and #oauth2.hasScope('read')")
 	public List<Categoria> listar(){		
 		return categoriaRepository.findAll();
 		
