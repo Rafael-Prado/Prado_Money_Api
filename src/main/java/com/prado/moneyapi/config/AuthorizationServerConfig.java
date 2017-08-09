255e0778d56974227df45113c41624cf605b840e
Rafae Prado <rafaeldacruzdoprado@hotmail.com>
2017-08-09 11:38:41 -0300

    Configurando o token para enviar usuário logado para o angular

--- src/main/java/com/prado/moneyapi/config/AuthorizationServerConfig.java
+++ src/main/java/com/prado/moneyapi/config/AuthorizationServerConfig.java
@@ -1,18 +1,26 @@
 package com.prado.moneyapi.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 
+import com.prado.moneyapi.config.token.CustomTokenEnhancer;
+
+@Profile("oauth-security")
 @Configuration
 @EnableAuthorizationServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -41,13 +49,16 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+		tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer(), accessTokenConverter()));
+		
 		endpoints
 			.tokenStore(tokenStore())
-			.accessTokenConverter(accessTokenConverter())
-			.reuseRefreshTokens(false)			
-			.authenticationManager(authenticationManager);	
-	}
-	
+			.tokenEnhancer(tokenEnhancerChain)
+			.reuseRefreshTokens(false)
+			.authenticationManager(authenticationManager);
+	}	
+
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter(){
 		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
@@ -58,5 +69,10 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Bean
 	public TokenStore tokenStore(){
 		return new JwtTokenStore(accessTokenConverter());
+	}
+	
+	@Bean
+	public TokenEnhancer  tokenEnhancer() {		
+		return new CustomTokenEnhancer();
 	}
 }
